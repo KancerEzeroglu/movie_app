d796930efaba33859f9ef15de9bac55261d6686e
KancerEzeroglu <ezeroglukancer@gmail.com>
2019-05-25 23:46:21 +0200

    Added video play feature.

--- js/get.js
+++ js/get.js
@@ -15,7 +15,8 @@ function clearFilms() {
         filmList.removeChild(filmList.firstChild);
     }
 }
-function fetchAndRender(page, searchText){
+
+function fetchAndRender(page, searchText) {
     httpGet(`http://www.omdbapi.com/?s=${searchText}&apikey=338f9a63&page=${page}`)
         .then(response => renderFilms(response))
         .catch(error => {
@@ -31,13 +32,12 @@ document.getElementById('search-button').addEventListener("click", function (eve
 });
 
 
-
 document.addEventListener("DOMContentLoaded", function (event) {
     fetchAndRender(currentPage, currentText);
 });
 
 
-window.onscroll = function(ev) {
+window.onscroll = function (ev) {
     if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
         // you're at the bottom of the page
         currentPage++;
@@ -47,15 +47,42 @@ window.onscroll = function(ev) {
 
 
 function renderFilms(response) {
-        var filmList = document.getElementById('film-list');
-        var result = JSON.parse(response);
+    var filmList = document.getElementById('film-list');
+    var result = JSON.parse(response);
 
-        result.Search.map(film => {
-            filmList.insertAdjacentHTML("beforeend", "<div class=\"film\">\n" +
-                "        <img id=\"film-img\" class=\"film-img\"\n" +
-                "             src=\"" + film.Poster + "\"\n" +
-                "             alt=\"Italian Trulli\">\n" +
-                "        <div class=\"film-title\" id=\"film-title\">"+film.Title+"</div>\n" +
-                "    </div>");
-        })
+    result.Search.map(film => {
+        filmList.insertAdjacentHTML("beforeend", "<div class=\"film\" onclick=\"lightbox_open();\">\n" +
+            "        <img id=\"film-img\" class=\"film-img\"\n" +
+            "             src=\"" + film.Poster + "\"\n" +
+            "             alt=\"Italian Trulli\">\n" +
+            "        <div class=\"film-overlay\"</div>\n" +
+            "        <div class=\"film-title\" id=\"film-title\">" + film.Title + "</div>\n" +
+            "    </div>");
+    })
+}
+
+
+window.document.onkeydown = function(e) {
+    if (!e) {
+        e = event;
+    }
+    if (e.keyCode == 27) {
+        lightbox_close();
+    }
+}
+
+function lightbox_open() {
+    var lightBoxVideo = document.getElementById("video-player");
+    window.scrollTo(0, 0);
+    document.getElementById('light').style.display = 'block';
+    document.getElementById('fade').style.display = 'block';
+    lightBoxVideo.currentTime = 0;
+    lightBoxVideo.play();
+}
+
+function lightbox_close() {
+    var lightBoxVideo = document.getElementById("video-player");
+    document.getElementById('light').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+    lightBoxVideo.pause();
 }
